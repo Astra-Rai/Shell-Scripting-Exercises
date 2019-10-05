482e0d2a59e9ac367e6246fd34de8ba090243b03
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-10-05 14:39:13 -0400

    Part2: Variables

---
+++ variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#YouTube Tutorial | Derek Banas
+#Declaring Variabes 
+#7:30-8:15/59:11
+
+#Notes:
+
+#declaring variables
+#no white space on sides of equal sign
+
+MYNAME="Astra Rai"
+
+#print variable, variable name enclosed in curly braces with $ dollar sign in front
+
+echo ${MYNAME}
