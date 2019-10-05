b7f40c9cb03def1b290601e148fe06de808705ca
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-10-05 15:15:14 -0400

    Part 4: Modulus | Exponents

---
+++ modulus-exponents.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#YouTube Tutorial | Derek Banas
+#Declaring a constants, math operations: +, -, *, /
+#10:06-10:36/59:11
+
+#raise the base 5 to the 2nd power
+echo $((5**2))
+
+#calculate the modulus of 5 divided by 4 
+echo $((5%4))
+
