248ac5d68b3839d66d00bda34074cb1a6a22ac2d
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-10-05 14:34:09 -0400

    Part 1: Hello World | Permission 755 Explained
    Hello World  string, echo command
    File permissions discussed
    Used the following in terminal to execute file: 
    chmod 755 helloworld.sh
    ./helloworld.sh
    
    permission 755 explained

---
+++ helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#YouTube Tutorial | Derek Banas
+#Source link: https://www.youtube.com/watch?v=hwrnmQumtPw&t=3143s
+#Hello World
+#5:00-7:30/59:11
+
+
+
+echo "Hello fam, Astra Rai in the house!"
