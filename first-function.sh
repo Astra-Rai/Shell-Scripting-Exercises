cc828847d4a7764c14b2e5d3683a594ceed45680
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-10-07 01:58:03 -0400

    Part 6: Introduction to Functions

---
+++ first-function.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#YouTube Tutorial | Derek Banas
+#Introduction to functions
+#15:15-16:00/59:11
+
+#name function getDat3
+#getDate() function will display the current time and date when called. 
+
+
+getDate(){
+    #date is a built
+    
+    date 
+    
+    return
+}
+
+#call function getDate
+
+getDate
+
