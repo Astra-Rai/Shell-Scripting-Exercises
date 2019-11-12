6942e0f93a52d9bf92b57e532564aba7d168cc5a
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-11-12 15:40:41 -0500

    Part 26: while loop

---
+++ while-loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#YouTube Tutorial | Derek Banas
+#Find string length
+#42:33-44:38/59:11
+
+num=1;
+ #while number is less than or equal to 10...
+ while [ $num -le 10 ]; do
+ #print value of variable num
+ echo $num
+ #increment num by one
+ num=$((num + 1))
+#end while loop
+done
+
