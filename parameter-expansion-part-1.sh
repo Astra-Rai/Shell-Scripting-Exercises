5b54a396002e1a52cff017f0301c491b1b8a5b6e
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-11-11 21:25:13 -0500

    Part 19: Parameter Expansion, Add char to string

---
+++ parameter-expansion-part-1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#YouTube Tutorial | Derek Banas
+#parameter expansions add character to end of string
+#36:23-37:29/59:11
+
+name="Derek";
+
+#use parameter expansion to add 's after the string Derek
+# ${indicates use of parameter expansion}
+
+echo "${name}'s toy "
+
