301c9f053bd32bdecc8d65d463f818169b22e39e
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-11-12 15:15:51 -0500

    Part 24: Counts String Characters

---
+++ count-string-characters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#YouTube Tutorial | Derek Banas
+#Find string length
+#42:00-42:33/59:11
+
+random_string="This is a random string"
+echo "Random string length is ${#random_string} characters"
+
+
