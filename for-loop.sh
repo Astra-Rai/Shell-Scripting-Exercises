4e78738647907b36ec6a51ea2bf1ea93c7b7f8dd
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-11-15 10:29:33 -0500

    Part 28: for loop

---
+++ for-loop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#YouTube Tutorial | Derek Banas
+#Find string length
+#49:43-50:18/59:11
+
+#increase value of i until value is less than or equal to 1
+for (( i=0; i <= 10; i=i+1 )) do 
+    echo $i
+
+#end of for loop
+done
